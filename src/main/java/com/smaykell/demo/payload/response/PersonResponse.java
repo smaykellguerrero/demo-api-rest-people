dec597ab5af3b35cf29ea060fef2b78cc8f25135
smaykellguerrero <smaykellguerrero@gmail.com>
2020-08-12 11:55:11 -0500

    updating date format and server port

--- src/main/java/com/smaykell/demo/payload/response/PersonResponse.java
+++ src/main/java/com/smaykell/demo/payload/response/PersonResponse.java
@@ -2,6 +2,8 @@ package com.smaykell.demo.payload.response;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,6 +17,7 @@ public class PersonResponse {
 
     private String gender;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date birthDate;
 
     private Double weight;
